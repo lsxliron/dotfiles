689734d5283ee256da7a4ef6ae51d613ccd1f155
lsxliron <lsxliron@noreply.com>
2025-02-21 00:54:36 -0500

    cleanup

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,17 +4,4 @@ return {
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = { "<c-k>", mode = { "i", "n" }, false }
   end,
-  -- opts = {
-  --   servers = {
-  --     pyright = {},
-  --     elixirls = {
-  --       cmd = { "/Users/lsxliron/.local/share/nvim/mason/bin/elixir-ls" },
-  --       filetypes = { "elixir", "eelixir", "heex", "surface" },
-  --     },
-  --     tailwindcss = {},
-  --     cssls = {},
-  --     vtsls = {},
-  --     lua_ls = {},
-  --   },
-  -- },
 }
