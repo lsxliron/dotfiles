20b761bf02fa14e01f9a1fb9bdd4f540dab52d06
lsxliron <lsxliron@noreply.com>
2025-03-02 23:57:01 -0500

    updated lsp-config it update on insert, enabled codelens

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,10 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
+  opts = function(_, opts)
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = { "<c-k>", mode = { "i", "n" }, false }
+
+    opts.diagnostics.update_in_insert = true
+    opts.codelens.enabled = true
   end,
 }
