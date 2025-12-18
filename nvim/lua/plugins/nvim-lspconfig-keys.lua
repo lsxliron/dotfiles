bf68dd6bffccd5c8485db838bcc6a9e8316c95ca
lsxliron <lsxliron@noreply.com>
2025-12-18 01:18:38 -0500

    fixed ctrl-k to get function info
    Signed-off-by: lsxliron <lsxliron@noreply.com>

--- nvim/lua/plugins/nvim-lspconfig-keys.lua
+++ nvim/lua/plugins/nvim-lspconfig-keys.lua
@@ -1,7 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function(opts)
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "<c-k>", mode = { "i", "n" }, false }
-  end,
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "<c-k>", mode = { "i", "n" }, false },
+        },
+      },
+    },
+  },
 }
