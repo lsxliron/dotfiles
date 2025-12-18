3d9e98251b50f59b356bc4c46818ef81e3d14207
lsxliron <lsxliron@noreply.com>
2025-12-18 01:18:59 -0500

    added OpenAI support
    Signed-off-by: lsxliron <lsxliron@noreply.com>

--- nvim/lua/plugins/code-companion.lua
+++ nvim/lua/plugins/code-companion.lua
@@ -7,6 +7,22 @@ return {
   },
   config = function()
     require("codecompanion").setup({
+      adapters = {
+        http = {
+          openai = function()
+            return require("codecompanion.adapters").extend("openai_responses", {
+              env = {
+                api_key = "OPENAI_API_KEY",
+              },
+              schema = {
+                model = {
+                  default = "gpt-4o",
+                },
+              },
+            })
+          end,
+        },
+      },
       extensions = {
         mcphub = {
           callback = "mcphub.extensions.codecompanion",
